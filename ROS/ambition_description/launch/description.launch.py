c907b14cc3ae75411a8936493f0069752ecac16f
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2026-03-09 20:48:11 +0100

    [ROS] Move ambition_description project from separate repo into Ambition repo

---
+++ ROS/ambition_description/launch/description.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import FileContent, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf = ParameterValue(
+        FileContent(PathJoinSubstitution([
+            FindPackageShare('ambition_description'),
+            'urdf',
+            'robot.urdf'
+        ])),
+        value_type=str
+    )
+    
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': urdf}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', PathJoinSubstitution([
+                FindPackageShare('ambition_description'),
+                'rviz',
+                'view.rviz'
+            ])]
+        )
+    ])
