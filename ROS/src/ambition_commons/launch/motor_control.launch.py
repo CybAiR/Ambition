e893b94e531536b436f203ce5eab659641411dab
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2026-04-23 22:30:00 +0000

    feat: motor_control launchfile launches md_node now

--- ROS/src/ambition_commons/launch/motor_control.launch.py
+++ ROS/src/ambition_commons/launch/motor_control.launch.py
@@ -1,8 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    candle_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('candle_ros2'),
+                'launch',
+                'md_node_launch.py'
+            )
+        )
+    )
+    
     motor_node = Node(
         package='ambition_control',
         executable='motor_control',
@@ -11,5 +24,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        candle_launch,
         motor_node
     ])
