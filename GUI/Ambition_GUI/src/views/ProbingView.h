87385e1d05bafd686257d8461ce9aa6d66c4569c
Joanna Zubik <joannazubik777@gmail.com>
2026-05-09 11:51:18 +0200

    feat: added camera view

--- GUI/Ambition_GUI/src/views/ProbingView.h
+++ GUI/Ambition_GUI/src/views/ProbingView.h
@@ -3,49 +3,51 @@
 #include "View.h"
 #include <array>
 
-class ProbingView : public View {
-public:
+class ProbingView : public View
+{
+  public:
     ProbingView(ImGuiWindowFlags flags = 0);
 
-    struct MissionProgress {
+    struct MissionProgress
+    {
         int probes_collected = 3;
         int target = 5;
-
     };
 
-    struct State {
+    struct State
+    {
         ArmTelemetry arm;
         GripperTelemetry gripper;
         MissionProgress mission;
 
-        std::array<bool, 3> container_filled = {
-            false,
-            false,
-            false
-        };
+        std::array<bool, 3> container_filled = {false, false, false};
     };
 
-public:
-    void Render(); 
+  public:
+    void Render();
     void Render(const State& state) const;
 
-    State& Data() noexcept { return state_; }
-    const State& Data() const noexcept { return state_; }
+    State& Data() noexcept
+    {
+        return state_;
+    }
+    const State& Data() const noexcept
+    {
+        return state_;
+    }
 
-private:
+  private:
+    void RenderLeftColumn(const State& state, float width) const;
     void RenderRightColumn(const State& state, float width) const;
-    void RenderArmTelemetryCard(
-        const ArmTelemetry& arm,
-        const GripperTelemetry& gripper
-    ) const;
-    
+    void RenderArmTelemetryCard(const ArmTelemetry& arm, const GripperTelemetry& gripper) const;
+
     // Add this declaration
     void RenderMissionProgressCard(const MissionProgress& mission) const;
 
     static const char* ToString(GripperState state) noexcept;
 
-    void InnerSeparator() const; 
+    void InnerSeparator() const;
 
-private:
+  private:
     State state_;
 };
